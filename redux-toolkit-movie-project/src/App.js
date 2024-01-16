18f7ad353e41a150d70dbe6fb7ad70b58f8cb0aa
Mehar-Aziz <meharaziz.1614>
2024-01-16 17:49:30 +0500

    Add Routes

--- redux-toolkit-movie-project/src/App.js
+++ redux-toolkit-movie-project/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Home from './components/Home';
+import MovieDetails from './components/MovieDetails';
+import PageNotFound from './components/PageNotFound';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Redux Toolkit</h1>
-    </div>
+    <Router>
+     <Header />
+      <div className='container'>
+        <Switch>
+          <Route path='/'exact component={Home} />
+          <Route path='/movie/:imdbID' component={MovieDetails} />
+          <Route component={PageNotFound} />
+        </Switch>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
