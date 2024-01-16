9d547a9d372b402954568763096469e35dd298d5
Mehar-Aziz <meharaziz.1614>
2024-01-16 18:16:24 +0500

    Updated header

--- redux-toolkit-movie-project/src/components/Header.js
+++ redux-toolkit-movie-project/src/components/Header.js
@@ -1,12 +1,16 @@
-import React from 'react'
-import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import React from 'react';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import './Header.css';
 
 const Header = () => {
     return(
         <div className='header'>
             <Link to='/'>
                 <div className='logo'>Movie App</div>
-            </Link>       
+            </Link>
+            <div className='user-image'>
+                <img src='{user}' alt='user' />
+            </div>
         </div>
     )
 }
