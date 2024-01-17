ebd3861504a0bf91d1e9cdb4a1e2b4df28352b32
Mehar-Aziz <meharaziz.1614>
2024-01-17 22:40:27 +0500

    Update listing

--- redux-toolkit-movie-project/src/components/MovieListing.js
+++ redux-toolkit-movie-project/src/components/MovieListing.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getAllMovies } from "../features/movieSlice";
+import MovieCard from "./MovieCard";
+import './MovieListing.css'
 
 const MovieListing = ()=>{
+    const movies = useSelector(getAllMovies);
+    console.log(movies);
     return(
-        <div>
-            Movie listing
+        <div className="movie-wrapper">
+            <div className="movie-list">
+                <h2>Movies</h2>
+                <div className="movie-container">{movies && movies.length && movies.map(movie => <MovieCard key={movie.id} data={movie} />)}</div>
+            </div>
+            
         </div>
     )
 }
