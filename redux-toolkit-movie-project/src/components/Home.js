d5b7ccf053c4f2587effa608600b90921fea9106
Mehar-Aziz <meharaziz.1614>
2024-01-17 22:00:53 +0500

    Updated Home page

--- redux-toolkit-movie-project/src/components/Home.js
+++ redux-toolkit-movie-project/src/components/Home.js
@@ -2,12 +2,15 @@ import React, { useEffect } from "react";
 import movieaApi from "../common/apis/movieaApi";
 import { APIkey } from "../common/apis/MovieApiKey";
 import MovieListing from "./MovieListing";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../features/movieSlice";
 
 const Home = ()=>{
+    const dispatch = useDispatch();
     useEffect(() =>{
         const fetchMovies = async() => {
             const response = await movieaApi.get(`movie?api_key=${APIkey}`);
-            console.log(response.data.results);
+            dispatch(addMovies(response.data.results));
         };
         fetchMovies();
     }, [])
